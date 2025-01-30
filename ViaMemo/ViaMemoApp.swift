1bc558774ba0165706203cbccf42204e14d07c97
Erick Lima <erickcreimberg@gmail.com>
2025-01-30 14:31:03 -0300

    Initial Commit

---
+++ ViaMemo/ViaMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  ViaMemoApp.swift
+//  ViaMemo
+//
+//  Created by ERICK COSTA REIMBERG DE LIMA on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ViaMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
